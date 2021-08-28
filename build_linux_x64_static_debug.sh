d0ae51aa4181a4164fad5212b719800356ca073b
subatomicdev <subatomicdev@protonmail.com>
2021-08-28 17:23:13 +0100

    lib output build config, added debug build script

---
+++ build_linux_x64_static_debug.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+
+echo
+echo Preparing vcpkg
+echo
+
+cd vcpkg
+
+./bootstrap-vcpkg.sh
+
+
+echo
+echo Installing packages
+echo
+
+./vcpkg install boost-beast boost-json --triplet x64-linux
+
+
+echo
+echo Building
+echo
+
+cd ../binancebeast
+
+cmake . -DCMAKE_BUILD_TYPE=Debug && make
+
+
+echo
+echo Building
+echo
+
+
+echo
+echo Running Test
+echo 
+
+
+cd bin
+./firstbuildtest
+
+
+echo
+echo Done
+echo 
+
