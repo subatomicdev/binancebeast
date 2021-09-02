c22a24479e16ee178b629ee2da04975cbce5f3be
subatomicdev <subatomicdev@protonmail.com>
2021-09-02 22:07:17 +0100

    Added SPOT tests, changed to live exchange

--- binancebeast/tests/testwebsockets.cpp
+++ binancebeast/tests/testwebsockets.cpp
@@ -9,17 +9,27 @@
 using namespace bblib;
 using namespace bblib_test;
 
-std::filesystem::path g_keyFile;
+std::filesystem::path g_futuresKeyFile, g_spotKeyFile;
+
 
 
 
 class WsTest : public testing::Test
 {
+public:
+    WsTest(Market market) : m_market(market)
+    {
+
+    }
+
+    virtual ~WsTest () = default;
+
+
 protected:
 
     virtual void SetUp() override
     {
-        auto config = ConnectionConfig::MakeTestNetConfig(Market::USDM, g_keyFile);
+        auto config = ConnectionConfig::MakeLiveConfig(m_market, g_futuresKeyFile);
 
         m_bb.start(config);
     }
@@ -43,13 +53,21 @@ protected:
     bool m_dataError;
     bool m_alwaysExpectData = true;
     std::condition_variable m_cvHaveReply;
+    Market m_market;
 };
+
+
 
 
 /// Start a websocket, do not explicitly close.
 class NormalWsTest : public WsTest
 {
 protected:
+    NormalWsTest(Market market) : WsTest(market)
+    {
+
+    }
+
     bool runTest(const string& stream, const bool alwaysExpectData =true) override
     {
         bool dataError;
@@ -68,9 +86,31 @@ protected:
 };
 
 
+class FuturesUsdmTest : public NormalWsTest
+{
+public:
+    FuturesUsdmTest() : NormalWsTest(Market::USDM)
+    {
+    }
+};
+
+class SpotTest : public NormalWsTest
+{
+public:
+    SpotTest() : NormalWsTest(Market::SPOT)
+    {
+    }
+};
+
+
 /// Start a websocket, wait a few seconds, then close the stream.
 class DisconnectWsTest : public WsTest
 {
+public:
+    DisconnectWsTest() : WsTest(Market::USDM)
+    {
+    }
+
 protected:
     bool runTest(const string& stream, const bool alwaysExpectData = true) override
     {
@@ -107,24 +147,38 @@ protected:
 };
 
 
-TEST_F (NormalWsTest, aggregrateTrade) { EXPECT_TRUE(runTest("btcusdt@aggTrade")); }
-TEST_F (NormalWsTest, markPrice) { EXPECT_TRUE(runTest("btcusdt@markPrice@1s")); }
-TEST_F (NormalWsTest, markPriceForAll) { EXPECT_TRUE(runTest("!markPrice@arr@1s"));}
-TEST_F (NormalWsTest, klines) { EXPECT_TRUE(runTest("btcusdt@kline_15m"));}
-TEST_F (NormalWsTest, continuousContractKline) { EXPECT_TRUE(runTest("btcusdt_perpetual@continuousKline_1m"));}
-TEST_F (NormalWsTest, individualSymbolMiniTicker) { EXPECT_TRUE(runTest("btcusdt@miniTicker"));}
-TEST_F (NormalWsTest, allMarketTicker) { EXPECT_TRUE(runTest("!ticker@arr"));}
-TEST_F (NormalWsTest, individualSymbolBookTicker) { EXPECT_TRUE(runTest("btcusdt@bookTicker"));}
-TEST_F (NormalWsTest, allBookTicker) { EXPECT_TRUE(runTest("!bookTicker"));}
-TEST_F (NormalWsTest, liquidationOrder) { EXPECT_TRUE(runTest("btcusdt@forceOrder", false));}
-TEST_F (NormalWsTest, allMarketLiquidationOrder) { EXPECT_TRUE(runTest("!forceOrder@arr", false));}
-TEST_F (NormalWsTest, partialBookDepth) { EXPECT_TRUE(runTest("btcusdt@depth5@100ms"));}
-TEST_F (NormalWsTest, diffBookDepth) { EXPECT_TRUE(runTest("btcusdt@depth@100ms"));}
-TEST_F (NormalWsTest, compositeIndexSymbolInfo) { EXPECT_TRUE(runTest("defiusdt@compositeIndex"));}
+
+// Futures USDM
+TEST_F (FuturesUsdmTest, aggregrateTrade) { EXPECT_TRUE(runTest("btcusdt@aggTrade")); }
+TEST_F (FuturesUsdmTest, markPrice) { EXPECT_TRUE(runTest("btcusdt@markPrice@1s")); }
+TEST_F (FuturesUsdmTest, markPriceForAll) { EXPECT_TRUE(runTest("!markPrice@arr@1s"));}
+TEST_F (FuturesUsdmTest, klines) { EXPECT_TRUE(runTest("btcusdt@kline_15m"));}
+TEST_F (FuturesUsdmTest, continuousContractKline) { EXPECT_TRUE(runTest("btcusdt_perpetual@continuousKline_1m"));}
+TEST_F (FuturesUsdmTest, individualSymbolMiniTicker) { EXPECT_TRUE(runTest("btcusdt@miniTicker"));}
+TEST_F (FuturesUsdmTest, allMarketTicker) { EXPECT_TRUE(runTest("!ticker@arr"));}
+TEST_F (FuturesUsdmTest, individualSymbolBookTicker) { EXPECT_TRUE(runTest("btcusdt@bookTicker"));}
+TEST_F (FuturesUsdmTest, allBookTicker) { EXPECT_TRUE(runTest("!bookTicker"));}
+TEST_F (FuturesUsdmTest, liquidationOrder) { EXPECT_TRUE(runTest("btcusdt@forceOrder", false));}
+TEST_F (FuturesUsdmTest, allMarketLiquidationOrder) { EXPECT_TRUE(runTest("!forceOrder@arr", false));}
+TEST_F (FuturesUsdmTest, partialBookDepth) { EXPECT_TRUE(runTest("btcusdt@depth5@100ms"));}
+TEST_F (FuturesUsdmTest, diffBookDepth) { EXPECT_TRUE(runTest("btcusdt@depth@100ms"));}
+TEST_F (FuturesUsdmTest, compositeIndexSymbolInfo) { EXPECT_TRUE(runTest("defiusdt@compositeIndex"));}
 
 
 TEST_F (DisconnectWsTest, allBookTicker) { EXPECT_TRUE(runTest("!bookTicker"));}
+
  
+
+// SPOT
+TEST_F (SpotTest, aggregrateTrade) { EXPECT_TRUE(runTest("btcusdt@aggTrade")); }
+TEST_F (SpotTest, trade) { EXPECT_TRUE(runTest("btcusdt@trade")); }
+TEST_F (SpotTest, klines) { EXPECT_TRUE(runTest("btcusdt@kline_15m"));}
+TEST_F (SpotTest, individualSymbolMiniTicker) { EXPECT_TRUE(runTest("btcusdt@miniTicker"));}
+TEST_F (SpotTest, allMarketTicker) { EXPECT_TRUE(runTest("!ticker@arr"));}
+TEST_F (SpotTest, individualSymbolBookTicker) { EXPECT_TRUE(runTest("btcusdt@bookTicker"));}
+TEST_F (SpotTest, allBookTicker) { EXPECT_TRUE(runTest("!bookTicker"));}
+TEST_F (SpotTest, partialBookDepth) { EXPECT_TRUE(runTest("btcusdt@depth5@100ms"));}
+TEST_F (SpotTest, diffBookDepth) { EXPECT_TRUE(runTest("btcusdt@depth@100ms"));}
 
 
 
@@ -132,14 +186,15 @@ int main (int argc, char ** argv)
 {
     std::cout << "\n\nTest REST API\n\n";
     
-    if (argc != 2)
+    if (argc != 3)
     {   
         std::cout << "Usage, requires key file or keys:\n"
-                  << argv[0] << " <full path to keyfile>\n";
+                  << argv[0] << " <path to futures keyfile> <path to spot keyfile>\n";
         return 1;
     }
     
-    g_keyFile = std::filesystem::path{argv[1]};
+    g_futuresKeyFile = std::filesystem::path{argv[1]};
+    g_spotKeyFile = std::filesystem::path{argv[2]};
 
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();    
